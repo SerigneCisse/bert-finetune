dce7ee869b61c500b7667ea3397fade6d94750ac
Timothy Liu <tlkh.xms@gmail.com>
2019-08-20 01:34:04 +0800

    Added BERTLARGE launcher script

---
+++ launch.large.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# for internal use
+
+ngc batch run --name "Experiment 0.0001 Dataset" \
+--preempt RUNONCE --total-runtime 100000s --image "nvidian/sae/ai-lab:19.08-tf" --ace nv-us-west-2 --instance dgx1v.16g.1.norm --commandline \
+"bash -c 'export DATASET_PORTION=0.0001 && git clone https://github.com/NVAITC/bert-finetune && cd bert-finetune && git checkout experiments && python3 bertlarge_training.py'" \
+--result /results --org nvidian --team sae
+
+ngc batch run --name "Experiment 0.0005 Dataset" \
+--preempt RUNONCE --total-runtime 100000s --image "nvidian/sae/ai-lab:19.08-tf" --ace nv-us-west-2 --instance dgx1v.16g.1.norm --commandline \
+"bash -c 'export DATASET_PORTION=0.0005 && git clone https://github.com/NVAITC/bert-finetune && cd bert-finetune && git checkout experiments && python3 bertlarge_training.py'" \
+--result /results --org nvidian --team sae
+
+ngc batch run --name "Experiment 0.001 Dataset" \
+--preempt RUNONCE --total-runtime 100000s --image "nvidian/sae/ai-lab:19.08-tf" --ace nv-us-west-2 --instance dgx1v.16g.1.norm --commandline \
+"bash -c 'export DATASET_PORTION=0.001 && git clone https://github.com/NVAITC/bert-finetune && cd bert-finetune && git checkout experiments && python3 bertlarge_training.py'" \
+--result /results --org nvidian --team sae
+
+ngc batch run --name "Experiment 0.005 Dataset" \
+--preempt RUNONCE --total-runtime 100000s --image "nvidian/sae/ai-lab:19.08-tf" --ace nv-us-west-2 --instance dgx1v.16g.1.norm --commandline \
+"bash -c 'export DATASET_PORTION=0.005 && git clone https://github.com/NVAITC/bert-finetune && cd bert-finetune && git checkout experiments && python3 bertlarge_training.py'" \
+--result /results --org nvidian --team sae
+
+ngc batch run --name "Experiment 0.01 Dataset" \
+--preempt RUNONCE --total-runtime 100000s --image "nvidian/sae/ai-lab:19.08-tf" --ace nv-us-west-2 --instance dgx1v.16g.1.norm --commandline \
+"bash -c 'export DATASET_PORTION=0.01 && git clone https://github.com/NVAITC/bert-finetune && cd bert-finetune && git checkout experiments && python3 bertlarge_training.py'" \
+--result /results --org nvidian --team sae
