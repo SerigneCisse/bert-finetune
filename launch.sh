e6981efeed357d56858d821c515d286601cba52c
Timothy Liu <tlkh.xms@gmail.com>
2019-08-20 06:39:50 +0800

    Add job running script

---
+++ launch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# for internal use
+
+ngc batch run --name "BERTBASE dbpedia 32GB" \
+--preempt RUNONCE --total-runtime 10000s --image "nvidian/sae/ai-lab:19.08-tf" --ace nv-us-west-2 --instance dgx1v.32g.8.norm --commandline \
+"bash -c 'export N_GPU=8 && git clone --depth 1 https://github.com/NVAITC/bert-finetune && cd bert-finetune && bash train_dbpedia_bertbase.sh'" \
+--result /results --org nvidian --team sae
+
+ngc batch run --name "BERTBASE dbpedia 16GB" \
+--preempt RUNONCE --total-runtime 10000s --image "nvidian/sae/ai-lab:19.08-tf" --ace nv-us-west-2 --instance dgx1v.16g.8.norm --commandline \
+"bash -c 'export N_GPU=8 && git clone --depth 1 https://github.com/NVAITC/bert-finetune && cd bert-finetune && bash train_dbpedia_bertbase.sh'" \
+--result /results --org nvidian --team sae
+
+ngc batch run --name "BERTLARGE dbpedia 32GB" \
+--preempt RUNONCE --total-runtime 10000s --image "nvidian/sae/ai-lab:19.08-tf" --ace nv-us-west-2 --instance dgx1v.32g.8.norm --commandline \
+"bash -c 'export N_GPU=8 && git clone --depth 1 https://github.com/NVAITC/bert-finetune && cd bert-finetune && bash train_dbpedia_bertlarge.sh'" \
+--result /results --org nvidian --team sae
+
+ngc batch run --name "BERTLARGE dbpedia 16GB" \
+--preempt RUNONCE --total-runtime 10000s --image "nvidian/sae/ai-lab:19.08-tf" --ace nv-us-west-2 --instance dgx1v.16g.8.norm --commandline \
+"bash -c 'export N_GPU=8 && git clone --depth 1 https://github.com/NVAITC/bert-finetune && cd bert-finetune && bash train_dbpedia_bertlarge.sh'" \
+--result /results --org nvidian --team sae
+
